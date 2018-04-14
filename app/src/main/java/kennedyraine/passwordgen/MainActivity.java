60f8d049da6ac1308ae18763c730d0bfa60f727e
kenn65lt <kmaranion@gmail.com>
2018-04-14 17:47:02 -0500

    RealCommit

--- app/src/main/java/kennedyraine/passwordgen/MainActivity.java
+++ app/src/main/java/kennedyraine/passwordgen/MainActivity.java
@@ -1,99 +1,113 @@
 package kennedyraine.passwordgen;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    TextView txt;
-    Button btn;
-    private RadioGroup passLengthGroup;
-     private RadioButton radio8;
-    private RadioButton radio16;
-    private RadioButton radio32;
-    private RadioButton radio64;
+import java.lang.Object;
+import java.util.ArrayList;
 
-    double num8;
-    double num16;
-    double num32;
-    double num64;
-    int passwordLength;
+public class MainActivity extends AppCompatActivity{
+
+    TextView txt, pWordTxt;
+    Button btn, copyBtn;
+
+//    int passwordLength = 0;
+//    String spinnerText = "";
+int passwordLength;
+    CheckBox numsCheck;
+    CheckBox specialsCheck;
+    CheckBox upperCheck;
+    CheckBox lowerCheck;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final RandomGenerate generate = new RandomGenerate(passwordLength);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txt = (TextView) findViewById(R.id.TextView);
         btn = (Button) findViewById(R.id.randomBtn);
+        Spinner spinner = (Spinner) findViewById(R.id.password_length);
+        RandomGenerate generate = new RandomGenerate();
 
-        passLengthGroup = (RadioGroup) findViewById(R.id.passLengthGroup);
-        radio8 = (RadioButton) findViewById(R.id.radio8);
-        radio16 = (RadioButton) findViewById(R.id.radio16);
-        radio32 = (RadioButton) findViewById(R.id.radio32);
-        radio64 = (RadioButton) findViewById(R.id.radio64);
 
-        //Checkbox Values
-        
-        //Listener for RadioButtons
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.password_length, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+
+
         btn.setOnClickListener(new View.OnClickListener() {
+            Spinner spin = (Spinner) findViewById(R.id.password_length);
+
+
             @Override
             public void onClick(View view) {
-                int checkedBox = passLengthGroup.getCheckedRadioButtonId();
-
-                switch(checkedBox){
-                    case R.id.radio8:
-                        if (radio8.isChecked()){
-                            passwordLength = 8;
-//                            String d = Double.toString(passwordLength);
-//                            txt.setText(d);
-                            generate.getLowerLetters(passwordLength/4);
-                            generate.getNum(passwordLength/4);
-                            generate.getSpecial(passwordLength/4);
-                            generate.getUpperLetters(passwordLength/4);
-
-
-                        }
-                            break;
-                    case R.id.radio16:
-                        if (radio16.isChecked()) {
-                            passwordLength = 16;
-                            generate.getLowerLetters(passwordLength/4);
-                            generate.getNum(passwordLength/4);
-                            generate.getSpecial(passwordLength/4);
-                            generate.getUpperLetters(passwordLength/4);
-                        }
-                        break;
-                    case R.id.radio32:
-                        if (radio32.isChecked()){
-                            passwordLength = 32;
-                            generate.getLowerLetters(passwordLength/4);
-                            generate.getNum(passwordLength/4);
-                            generate.getSpecial(passwordLength/4);
-                            generate.getUpperLetters(passwordLength/4);
-                        }
-                        break;
-                    case R.id.radio64:
-                        if (radio64.isChecked()){
-                            passwordLength = 64;
-                            generate.getLowerLetters(passwordLength/4);
-                            generate.getNum(passwordLength/4);
-                            generate.getSpecial(passwordLength/4);
-                            generate.getUpperLetters(passwordLength/4);
-                        }
-                        break;
-
+                RandomGenerate generate = new RandomGenerate();
+                numsCheck = (CheckBox) findViewById(R.id.numCheck);
+                if (numsCheck.isChecked()){
+                    String nums = "1234567890";
+                    generate.setNum(nums);
+                } else{
+                    generate.setNum("");
                 }
+                upperCheck = (CheckBox) findViewById(R.id.upperCheck);
+                if (upperCheck.isChecked()){
+                    String upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                    generate.setUpperLetters(upperLetters);
+                }else{
+                    generate.setSpecials("");
+                }
+                lowerCheck = (CheckBox) findViewById(R.id.lowerCheck);
+                if (lowerCheck.isChecked()){
+                    String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+                    generate.setLowers(lowerCase);
+                }else{
+                    generate.setLowers("");
+                }
+                specialsCheck = (CheckBox) findViewById(R.id.specialsCheck);
+                if (specialsCheck.isChecked()){
+                    String specials = "!@#$%&*()_+-=[]|,./?><";
+                    generate.setSpecials(specials);
+                }else{
+                    generate.setSpecials("");
+                }
+
+                String passwordString = spin.getSelectedItem().toString();
+                //Parsing string password to int
+                 passwordLength = Integer.parseInt(passwordString);
+                 generate.setPassword(passwordLength);
+
+                 txt.setText(generate.getMerged());
+//                 txt.setText(generate.getMerged());
 
             }
         });
 
 
     }
-//    EditText e = (EditText) findViewById(R.id.editText);
-//    e.("New Text");
+
+
+
+
 }
--- app/src/main/java/kennedyraine/passwordgen/RandomGenerate.java
+++ app/src/main/java/kennedyraine/passwordgen/RandomGenerate.java
@@ -3,111 +3,68 @@ package kennedyraine.passwordgen;
 import java.util.Random;
 
 
-public class RandomGenerate {
+public class RandomGenerate extends MainActivity {
     public int pWordLength = 0;
-   public int characterCount =0;
+    public int characterCount = 0;
 
+    String lowerCase = "";
+    String upperLetters = "";
+    String nums = "";
+    String specials = "";
 
     //constructors
-    RandomGenerate(int pWordLength ){
+
+    public RandomGenerate() {
+    }
+
+    public void setPassword(int pWordLength){
         this.pWordLength = pWordLength;
-        characterCount = pWordLength /4;
+    }
+    public int getPassword(){
+        return this.pWordLength;
     }
 
+    //Setter for lower letters
+    public void setLowers(String lowerCase){
+        this.lowerCase = lowerCase;
+    }
+    public String getLowerLetters(){ return this.lowerCase; }
 
-    public String getNum(int length){
-        String digits = "1234567890";
+    //Setter and getter for upper letters
+    public void setUpperLetters(String upperLetters){
+        this.upperLetters = upperLetters;
+    }
+    public String getUpperLetters(){ return this.upperLetters;}
 
-        StringBuilder nums = new StringBuilder();
+    //Setter and getter for Special letters
+    public void setSpecials(String specials){
+        this.specials = specials;
+    }
+    public String getSpecial(){return this.specials;}
+
+    //Setters and getters for nums
+    public void setNum(String nums){
+        this.nums = nums;
+    }
+    public String getNums(){return this.nums;}
+
+
+    public String getMerged() {
+//        String merged = getLowerLetters() + getSpecial() + getUpperLetters() + getNums();
+        String merged = nums + upperLetters + lowerCase+ getSpecial();
+        StringBuilder chars = new StringBuilder();
         Random rnd = new Random();
-        while (nums.length() < length) { // length of the random string.
-            int index = (int) (rnd.nextFloat() * digits.length());
-            nums.append(digits.charAt(index));
+
+        while (chars.length() < getPassword()) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * merged.length());
+            chars.append(merged.charAt(index));
         }
-        return nums.toString();
+        return chars.toString();
     }
 
 
 
-
-    //Counting the percentages of the characters that are going to be put on
-    //Getters and setters for the percentages
-    //Returns random numbers
-    Random rndm_method = new Random();
-    public String getUpperLetters(int length){
-        String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-        StringBuilder uppers = new StringBuilder();
-        Random rnd = new Random();
-        while (uppers.length() < length) { // length of the random string.
-            int index = (int) (rnd.nextFloat() * upperCase.length());
-            uppers.append(upperCase.charAt(index));
-        }
-        return uppers.toString();
-//        return randomLetters.toString();
-    }
-
-
-
-    public String getLowerLetters(int length){
-
-
-          String lowerCase = "abcdefghijklmnopqrstuvwxyz";
-
-        StringBuilder lowers = new StringBuilder();
-        Random rnd = new Random();
-            while (lowers.length() < length) { // length of the random string.
-                int index = (int) (rnd.nextFloat() * lowerCase.length());
-                lowers.append(lowerCase.charAt(index));
-            }
-            return lowers.toString();
-        }
-
-
-
-    public String getSpecial(int length){
-
-        String specialLetters = "!@#$%&*()_+-=[]|,./?><";
-        StringBuilder specials = new StringBuilder();
-        Random rnd = new Random();
-        while (specials.length() < length) { // length of the random string.
-            int index = (int) (rnd.nextFloat() * specialLetters.length());
-            specials.append(specialLetters.charAt(index));
-        }
-        return specials.toString();
-    }
-
-//    //Generates random Number based on the percentage
-    private int getRandomNumber(int chars){//Chars is the length of each characters
-
-        int randomInt = 0;
-        Random randomGen = new Random();
-        randomInt = randomGen.nextInt(chars);
-        if (randomInt - 1 == -1){
-            return randomInt;
-        } else {
-            return randomInt - 1;
-        }
-
-    }
 }
-//    private String setMergeCharacters(){
-//        String characters = getLetters() + getLowerLetters() + getNum() + getSpecials();
-//        StringBuilder merge = new StringBuilder(characters);
-//
-//        for (int x = 0; x <= pWordLength; x++){
-//            int number = getRandomNumber(characters.length());
-//            char specs = characters.charAt(number);
-//            merge.append(specs);
-//        }
-//        return merge.toString();
-//
-//
-//    }
 
-//    public String getMergedChar(){
-//        return setMergeCharacters();
-//    }
-//}
 
 
